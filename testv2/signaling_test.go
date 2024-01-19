0c9ac72a5f02f3aa45e2f987541920a75c23254a
pagefau1t <pagefau1t@gmail.com>
2024-01-19 15:51:17 +0800

    fix: signal invite add channelID return
    Signed-off-by: pagefau1t <pagefau1t@gmail.com>

--- testv2/signaling_test.go
+++ testv2/signaling_test.go
@@ -5,22 +5,21 @@ import (
 	"testing"
 
 	"github.com/openimsdk/openim-sdk-core/v3/open_im_sdk"
-	"github.com/openimsdk/openim-sdk-core/v3/pkg/constant"
-	// "github.com/openimsdk/openim-sdk-core/v3/pkg/constant"
 )
 
 func Test_SignalingInvite(t *testing.T) {
 	ctx = context.WithValue(ctx, "callback", TestSendMsg{})
 	inviteUserIDs := []string{"6512ba81b6f8463c81a0ab6a"}
-	err := open_im_sdk.UserForSDK.Signaling().SignalingInvite(ctx, inviteUserIDs)
+	channelID, err := open_im_sdk.UserForSDK.Signaling().SignalingInvite(ctx, "sg_10010", inviteUserIDs)
 	if err != nil {
 		t.Fatal(err)
 	}
+	t.Log(channelID)
 }
 
 func Test_SignalingAccept(t *testing.T) {
 	ctx = context.WithValue(ctx, "callback", TestSendMsg{})
-	err := open_im_sdk.UserForSDK.Signaling().SignalingAccept(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3")
+	err := open_im_sdk.UserForSDK.Signaling().SignalingAccept(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", "")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -28,7 +27,7 @@ func Test_SignalingAccept(t *testing.T) {
 
 func Test_SignalingReject(t *testing.T) {
 	ctx = context.WithValue(ctx, "callback", TestSendMsg{})
-	err := open_im_sdk.UserForSDK.Signaling().SignalingReject(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", constant.SingleChatType)
+	err := open_im_sdk.UserForSDK.Signaling().SignalingReject(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", "")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -36,7 +35,7 @@ func Test_SignalingReject(t *testing.T) {
 
 func Test_SignalingJoin(t *testing.T) {
 	ctx = context.WithValue(ctx, "callback", TestSendMsg{})
-	err := open_im_sdk.UserForSDK.Signaling().SignalingJoin(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", "xadxwr24")
+	err := open_im_sdk.UserForSDK.Signaling().SignalingJoin(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", "")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -44,7 +43,7 @@ func Test_SignalingJoin(t *testing.T) {
 
 func Test_SignalingCancel(t *testing.T) {
 	ctx = context.WithValue(ctx, "callback", TestSendMsg{})
-	err := open_im_sdk.UserForSDK.Signaling().SignalingCancel(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", constant.SingleChatType, "6513d4643eaa3004e476c620")
+	err := open_im_sdk.UserForSDK.Signaling().SignalingCancel(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", "", "10010")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -52,7 +51,7 @@ func Test_SignalingCancel(t *testing.T) {
 
 func Test_SignalingHungUp(t *testing.T) {
 	ctx = context.WithValue(ctx, "callback", TestSendMsg{})
-	err := open_im_sdk.UserForSDK.Signaling().SignalingHungUp(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", constant.SingleChatType)
+	err := open_im_sdk.UserForSDK.Signaling().SignalingHungUp(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", "")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -60,7 +59,7 @@ func Test_SignalingHungUp(t *testing.T) {
 
 func Test_SignalingClose(t *testing.T) {
 	ctx = context.WithValue(ctx, "callback", TestSendMsg{})
-	err := open_im_sdk.UserForSDK.Signaling().SignalingClose(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", constant.SingleChatType)
+	err := open_im_sdk.UserForSDK.Signaling().SignalingClose(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", "")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -68,7 +67,7 @@ func Test_SignalingClose(t *testing.T) {
 
 func Test_SignalingMicphoneStatusChange(t *testing.T) {
 	ctx = context.WithValue(ctx, "callback", TestSendMsg{})
-	err := open_im_sdk.UserForSDK.Signaling().MichoneStatusChange(ctx, "51cc96934c80801b8bb4f3c94f395fc0", 1)
+	err := open_im_sdk.UserForSDK.Signaling().MichoneStatusChange(ctx, "51cc96934c80801b8bb4f3c94f395fc0", "", 1)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -76,7 +75,7 @@ func Test_SignalingMicphoneStatusChange(t *testing.T) {
 
 func Test_SignalingSpeakStatusChange(t *testing.T) {
 	ctx = context.WithValue(ctx, "callback", TestSendMsg{})
-	err := open_im_sdk.UserForSDK.Signaling().SpeakStatusChange(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3")
+	err := open_im_sdk.UserForSDK.Signaling().SpeakStatusChange(ctx, "94f3d1d5f39fc49b3496cdb379d0d3f3", "")
 	if err != nil {
 		t.Fatal(err)
 	}
